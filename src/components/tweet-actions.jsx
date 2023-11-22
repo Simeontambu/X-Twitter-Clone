8dd9c253052f1f471875662cef9a446f16ed43d2
simeontambu <simeontambu@gmail.com>
2023-11-22 11:46:15 +0100

    update component tweet actions

--- src/components/tweet-actions.jsx
+++ src/components/tweet-actions.jsx
@@ -2,23 +2,23 @@ import { FiMessageCircle } from "react-icons/fi";
 import { FaRetweet } from "react-icons/fa6";
 import { CiHeart } from "react-icons/ci";
 import { FiShare } from "react-icons/fi";
-export default function Tweetactions(props) {
+export default function Tweetactions({userComment,userRetweet,userLove}) {
   return (
     <>
       <div className="tweet-actions">
         <div className="tweet-action">
           <FiMessageCircle />
-          {props.userComment}
+          {userComment}
         </div>
 
         <div className="tweet-action">
           <FaRetweet />
-          {props.userRetweet}
+          {userRetweet}
         </div>
 
         <div className="tweet-action">
-          <CiHeart />
-          {props.userLove}
+          <span><CiHeart />{userLove}</span>
+          
         </div>
         <div className="tweet-action">
           <FiShare />
