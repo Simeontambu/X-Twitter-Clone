d764999c16a9330c19f60bad6db67fb852ffb78e
simeontambu <simeontambu@gmail.com>
2024-02-14 09:25:38 +0100

    Update useMap

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -1,61 +1,59 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import fileJson from "./../data/initial-data copy.json";
-import tweeetPicture from "../../src/images/logo-rond-twitter.svg";
-import newYorkTime from "../../src/images/logoNewYorkTime.svg";
-import cnn from "../../src/images/tweet-profile-photo.svg";
-import axios from "axios";
+import { createContext, useContext, useState, useEffect } from "react"
+import fileJson from "./../data/initial-data copy.json"
+import tweeetPicture from "../../src/images/logo-rond-twitter.svg"
+import newYorkTime from "../../src/images/logoNewYorkTime.svg"
+import cnn from "../../src/images/tweet-profile-photo.svg"
+import axios from "axios"
 
-const DataContext = createContext();
+const DataContext = createContext()
 
 export function useData() {
-  const data = useContext(DataContext);
+  const data = useContext(DataContext)
 
-  return data;
+  return data
 }
 
 export function DataContextProvider({ children }) {
-  const [isLiked, setIsLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState(false)
+  const [data, setData] = useState(fileJson)
 
-  // Function to add tweet to tweet list
-  const [data, setData] = useState(fileJson);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true);
       try {
-        const tweetsResponse = await axios.get("http://localhost:8000/tweets");
+        const tweetsResponse = await axios.get("http://localhost:8000/tweets")
         const currentUserResponse = await axios.get(
           "http://localhost:8000/currentUser"
-        );
+        )
         const combinedData = {
           tweets: tweetsResponse.data,
           currentUser: currentUserResponse.data,
-        };
-        setData(combinedData);
+        }
+        setData(combinedData)
       } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+        console.error("Error fetching data:", error)
+      } 
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
+  // Function to add tweet to tweet list
   const addTweet = (newTweet) => {
     axios
       .post("http://localhost:8000/tweets", newTweet)
       .then((response) => {
-        console.log("data", response.data);
+        console.log("data", response.data)
 
         setData((data) => {
-          const updatedTweets = [response.data, ...data.tweets];
-          return { ...data, tweets: updatedTweets };
-        });
+          const updatedTweets = [response.data, ...data.tweets]
+          return { ...data, tweets: updatedTweets }
+        })
       })
       .catch((error) => {
-        console.error("Error posting tweet:", error);
-      });
-  };
+        console.error("Error posting tweet:", error)
+      })
+  }
 
   const value = {
     data,
@@ -63,11 +61,10 @@ export function DataContextProvider({ children }) {
     tweeetPicture,
     newYorkTime,
     cnn,
-    isLoading,
     isLiked,
 
     setIsLiked,
-  };
+  }
 
-  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>
 }
