a00cdc96065cd38459f1f45fa9038d35164c048f
simeontambu <simeontambu@gmail.com>
2024-03-15 20:52:27 +0100

    Feat: user setting style

--- src/components/sidebarleft/userSetting.jsx
+++ src/components/sidebarleft/userSetting.jsx
@@ -9,23 +9,23 @@ export default function UserSetting() {
   return (
     <>
       <NavLink to={`/${username}`} className="active">
-        <div className="user-setting">
+        <div className="flex items-center rounded-[4rem] hover:bg-[#1a1a1a] p-[0.7em]">
           <Image
             src={isLogin.profilePicture}
             className="flex-shrink-0 flex-grow-0 w-[48px] max-w-full max-h-[100px] rounded-full"
           />
-          <div className="">
-            <div className="user-setting-name">
-              <div className="user-setting-icone">
-                <span>{isLogin.name}</span>
 
+          <div className="flex gap-8">
+            <div>
+              <div className="flex gap-1">
+                <span className="">{isLogin.name}</span>
                 <Image src={Private} />
-                <span className="user-setting-three-dot">...</span>
               </div>
+              <span className=" text-gray-500">
+                {`${isLogin.handle}...`}...
+              </span>
             </div>
-            <span className=" tweet-title-details">
-              {`${isLogin.handle}...`}...
-            </span>
+            <span className="font-bold text-xl">...</span>
           </div>
         </div>
       </NavLink>
